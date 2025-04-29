32c600fd0190c2bca19fbe740012348919e26063
Alavala-Vinay <vinayalavala154@gmail.com>
2025-04-29 11:14:06 +0530

    handling error page

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useAppContext } from '../context/appContext.jsx'
 
 const ErrorPage = () => {
+
+    const {navigate} = useAppContext()
+    const handleGoBack = () => {
+        navigate('/')
+    }
+    const handleContactSupport = () => {
+        navigate('/contact')
+    }
   return (
     <div className='flex items-center justify-center '>
         <div class="flex flex-col items-center justify-center text-sm h-[400px]">
@@ -8,10 +17,10 @@ const ErrorPage = () => {
             <h2 class="md:text-6xl text-4xl font-semibold text-gray-800">Page Not Found</h2>
             <p class="text-base mt-4 text-gray-500">Sorry, we couldn’t find the page you’re looking for.</p>
             <div class="flex items-center gap-4 mt-6">
-                <button type="button" class="bg-primary hover:bg-primary-dull px-7 py-2.5 text-white rounded active:scale-95 transition-all">
+                <button onClick={()=>handleGoBack} type="button" class="bg-primary hover:bg-primary-dull px-7 py-2.5 text-white rounded active:scale-95 transition-all">
                     Go back home
                 </button>
-                <button type="button" class="group flex items-center gap-2 px-7 py-2.5 active:scale-95 transition">
+                <button onClick={()=>handleContactSupport} type="button" class="group flex items-center gap-2 px-7 py-2.5 active:scale-95 transition">
                     Contact support
                     <svg class="group-hover:translate-x-0.5 mt-1 transition" width="15" height="11" viewBox="0 0 15 11" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M1 5.5h13.092M8.949 1l5.143 4.5L8.949 10" stroke="#1F2937" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
