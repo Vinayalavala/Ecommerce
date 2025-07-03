56ee9575e60b751bb39722bec1d2b22d747c830e
Vinay alavala <147227489+Vinayalavala@users.noreply.github.com>
2025-07-03 12:28:36 +0530

    Add files via upload

---
+++ client/src/pages/seller/Analytics.jsx
@@ -0,0 +1,11 @@
+const Analytics = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Seller Analytics</h1>
+      <p>This page will visualize your orders, transactions, and income.</p>
+        <p>Currently, this feature is under development.</p>
+    </div>
+  );
+};
+
+export default Analytics;
