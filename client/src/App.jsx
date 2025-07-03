af60aa94d299e714463413a330faef17c3de0cc0
Vinay alavala <147227489+Vinayalavala@users.noreply.github.com>
2025-07-03 12:30:47 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,39 +18,45 @@ import SellerLayout from './pages/seller/SellerLayout.jsx';
 import AddProduct from './pages/seller/AddProduct.jsx';
 import ProductList from './pages/seller/ProductList';
 import Orders from './pages/seller/Orders';
+import Analytics from './pages/seller/Analytics.jsx';
 import Loading from './components/Loading';
 
 const App = () => {
 
   const isSellerPath = useLocation().pathname.includes('seller');
-  const {showUserLogin,isSeller} = useAppContext();
+  const { showUserLogin, isSeller } = useAppContext();
+
   return (
     <div className='text-default min-h-screen text-gray-700 bg-white'>
-      {isSellerPath? null :<Navbar />}
-      {showUserLogin?<Login/>:null}
-      <Toaster/>
-      <div className={`${ isSellerPath?"":"px-6 md:px-16 lg:px-24 xl:px-32"}`}>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/products' element={<AllProducts />} />
-        <Route path='/products/:category' element={<ProductCategory />} />
-        <Route path='/products/:category/:id' element={<ProductDetails />} />
-        <Route path='/contact' element={<ContactPage />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/add-address' element={<AddAdress />} />
-        <Route path='/my-orders' element={<MyOrders />} />
-        <Route path='/loader' element={<Loading />} />
-        <Route path='/seller' element={isSeller? <SellerLayout/>:<SellerLogin/>}> 
-          <Route index element={isSeller?<AddProduct/>:null}/>
-          <Route path='product-list' element={<ProductList/>}/>
-          <Route path='orders' element={<Orders/>}/>
-        </Route>
-        <Route path='*' element={<ErrorPage />} /> 
-    </Routes>
+      {isSellerPath ? null : <Navbar />}
+      {showUserLogin ? <Login /> : null}
+      <Toaster />
+      <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/products' element={<AllProducts />} />
+          <Route path='/products/:category' element={<ProductCategory />} />
+          <Route path='/products/:category/:id' element={<ProductDetails />} />
+          <Route path='/contact' element={<ContactPage />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/add-address' element={<AddAdress />} />
+          <Route path='/product/:id' element={<ProductDetails />} />
+          <Route path='/my-orders' element={<MyOrders />} />
+          <Route path='/loader' element={<Loading />} />
+          
+          <Route path='/seller' element={isSeller ? <SellerLayout /> : <SellerLogin />}>
+            <Route index element={isSeller ? <AddProduct /> : null} />
+            <Route path='product-list' element={<ProductList />} />
+            <Route path='orders' element={<Orders />} />
+            <Route path='analytics' element={<Analytics />} />
+          </Route>
+
+          <Route path='*' element={<ErrorPage />} />
+        </Routes>
       </div>
-      {!isSellerPath&&<Footer />}
+      {!isSellerPath && <Footer />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
