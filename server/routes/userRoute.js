4cc01b50586700f07781b44bb1fcdc2063386d3f
Vinay alavala <147227489+Vinayalavala@users.noreply.github.com>
2025-07-04 09:36:58 +0530

    Update userRoute.js

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,15 +1,19 @@
-import express from 'express'
-import { register,login,isAuth, logout } from '../controllers/userController.js';
+import express from 'express';
+import {
+  register,
+  login,
+  isAuth,
+  logout,
+  forgotPassword 
+} from '../controllers/userController.js';
 import authUser from '../middlewares/authUser.js';
 
+const userRouter = express.Router();
 
+userRouter.post('/register', register);
+userRouter.post('/login', login);
+userRouter.post('/forgot-password', forgotPassword); 
+userRouter.get('/is-auth', authUser, isAuth);
+userRouter.get('/logout', authUser, logout);
 
-const userRouter = express.Router()
-
-userRouter.post('/register',register)
-userRouter.post('/login',login)
-userRouter.get('/is-auth',authUser,isAuth)
-userRouter.get('/logout',authUser,logout)
-
-
-export default userRouter
+export default userRouter;
