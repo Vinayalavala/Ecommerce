bb26f037739fa833673a6114def90229ec2a3600
Vinay alavala <147227489+Vinayalavala@users.noreply.github.com>
2025-07-04 09:21:01 +0530

    Update MainBanner.jsx

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Mainbanner = () => {
   return (
-    <div className='relative mt-20'>
+    <div className='relative mt-24'>
         <img src={assets.main_banner_bg} alt="banner" className='w-full hidden md:block'/>
         <img src={assets.main_banner_bg_sm} alt="banner" className='w-full  md:hidden'/>
         <div className='absolute inset-0 flex flex-col items-center md:items-start justify-end md:justify-center pb-24 md:pb-0 px-4 md:pl-18 lg:pl-24 '>
