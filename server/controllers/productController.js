247ab2f56762536e6a2c7639920f26d33f6cf685
Vinay alavala <147227489+Vinayalavala@users.noreply.github.com>
2025-07-11 20:55:33 +0530

    Update productController.js

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -102,35 +102,6 @@ export const productById = async (req, res) => {
   }
 };
 
-// ---------------------------------------------
-// Change Stock Status
-// ---------------------------------------------
-export const changeStock = async (req, res) => {
-  try {
-    const { id, inStock } = req.body;
-
-    if (!id) {
-      return res.status(400).json({
-        success: false,
-        message: "Product ID is required",
-      });
-    }
-
-    await Product.findByIdAndUpdate(id, { inStock });
-
-    return res.status(200).json({
-      success: true,
-      message: "Stock updated successfully",
-    });
-
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      success: false,
-      message: error.message,
-    });
-  }
-};
 
 // ---------------------------------------------
 // Delete Product by ID
@@ -168,3 +139,4 @@ export const deleteProduct = async (req, res) => {
     });
   }
 };
+
