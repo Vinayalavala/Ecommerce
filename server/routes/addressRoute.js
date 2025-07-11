521a0015b92e45f854f79f0951ff63685231c07a
Vinay alavala <147227489+Vinayalavala@users.noreply.github.com>
2025-07-11 20:04:43 +0530

    Update addressRoute.js

--- server/routes/addressRoute.js
+++ server/routes/addressRoute.js
@@ -1,10 +1,24 @@
 import express from 'express';
 import authUser from '../middlewares/authUser.js';
-import { addAddress,getAddress } from '../controllers/addressController.js';
+import {
+  addAddress,
+  getAddress,
+  deleteAddress,
+  editAddress,
+} from '../controllers/addressController.js';
 
 const addressRouter = express.Router();
 
-addressRouter.post('/add',authUser,addAddress)
-addressRouter.get('/get',authUser,getAddress)
+// Create new address
+addressRouter.post('/add', authUser, addAddress);
+
+// Get all addresses for a user
+addressRouter.get('/get', authUser, getAddress);
+
+// Delete an address by ID
+addressRouter.delete('/delete/:id', authUser, deleteAddress);
+
+// Edit an existing address (optional)
+addressRouter.put('/edit/:id', authUser, editAddress);
 
 export default addressRouter;
