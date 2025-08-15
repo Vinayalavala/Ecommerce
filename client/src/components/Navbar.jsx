b58ae9a2c841dc764076a1a5e5d63cb07fb96cf0
Vinay alavala <147227489+Vinayalavala@users.noreply.github.com>
2025-08-15 21:35:30 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -230,8 +230,8 @@ const Navbar = () => {
         </div>
 
         {/* Mobile Search Bar */}
-        <div className={`lg:hidden mb-3 transition-all duration-300 ${hideTopBar ? 'flex justify-center items-center h-3' : 'mt-0'}`}>
-          <div className="flex items-center text-sm gap-2 border border-gray-300 px-3 py-1.4 rounded-full w-full max-w-md bg-white shadow-sm relative overflow-hidden">
+        <div className={`lg:hidden mb-3 transition-all duration-300 ${hideTopBar ? 'flex justify-center items-center h-2.98' : 'mt-0'}`}>
+          <div className="flex items-center text-sm gap-2 border border-gray-300 px-3 py-1.5 rounded-full w-full max-w-md bg-white shadow-sm relative overflow-hidden">
             <input
               onChange={(e) => setSearchQuery(e.target.value)}
               onFocus={() => setSearchFocused(true)}
