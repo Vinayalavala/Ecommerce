fb389d393a3a770d53a4f4d04dd5a8444106e2b3
Vinay alavala <147227489+Vinayalavala@users.noreply.github.com>
2025-08-18 10:13:20 +0530

    Update Product.js

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,15 +1,26 @@
 import mongoose from "mongoose";
 
+const mediaSchema = new mongoose.Schema(
+  {
+    url: { type: String, required: true },
+    public_id: { type: String, required: true },
+  },
+  { _id: false } // prevent automatic _id for each media item
+);
+
 const productSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     description: [{ type: String, required: true }],
     price: { type: Number, required: true },
     offerPrice: { type: Number, required: true },
-    image: [{ type: String }],
-    video: [{ type: String }],
+
+    image: [mediaSchema], // ✅ array of { url, public_id }
+    video: [mediaSchema], // ✅ array of { url, public_id }
+
     mainCategory: { type: String, required: true },
     category: { type: String, required: true },
+
     stock: { type: Number, required: true },
     inStock: { type: Boolean, default: true },
 
@@ -20,8 +31,7 @@ const productSchema = new mongoose.Schema(
 
     reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
   },
-  { timestamps: true,strict: false }
+  { timestamps: true, strict: false }
 );
 
 export default mongoose.model("Product", productSchema);
-
