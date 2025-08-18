2f369a72de52e7345eb52245846f482c5fd2b8b3
Vinay alavala <147227489+Vinayalavala@users.noreply.github.com>
2025-08-18 13:02:57 +0530

    Update adRoutes.js

--- server/routes/adRoutes.js
+++ server/routes/adRoutes.js
@@ -3,16 +3,16 @@ import { addAd, getAds, getActiveAds, updateAd, deleteAd } from "../controllers/
 
 const adRouter = express.Router();
 
-// Create ad (expects FormData with "media")
+// Create ad (expects JSON with base64 "media")
 adRouter.post("/", addAd);
 
-// List ads
+// List ads (?admin=true for all, optional ?placement=)
 adRouter.get("/", getAds);
 
 // Active ads only
 adRouter.get("/active", getActiveAds);
 
-// Update ad (optionally replace media)
+// Update ad (optionally replace media with base64 "media")
 adRouter.put("/:id", updateAd);
 
 // Delete ad
