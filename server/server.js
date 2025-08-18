e482703b95fbbe8a7879b8e7810c5c522ec2379f
Vinay alavala <147227489+Vinayalavala@users.noreply.github.com>
2025-08-18 13:03:38 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 4001;
 const allowedOrigins = [
   'http://localhost:5173',
   'https://padmavatimilkpoint.vercel.app',
-  'https://alavalasrootandcraft.vercel.app',
+  'https://vishwaadhika.vercel.app',
   'https://milkpoint.vercel.app'
 ];
 
@@ -35,8 +35,8 @@ app.use(cors({
 }));
 
 // Body parsers
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // Cookie parser
 app.use(cookieParser());
