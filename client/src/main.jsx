b64ce60c3295a88c1495a41804b9f331a8a4eee4
Vinay alavala <147227489+Vinayalavala@users.noreply.github.com>
2025-08-19 10:17:44 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
-import { AppContextProvider } from './context/appContext.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import { AppContextProvider } from "./context/appContext.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <AppContextProvider>
-        <App />
-    </AppContextProvider>
-  </BrowserRouter>,
-)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <AppContextProvider>
+        <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+          <App />
+        </GoogleOAuthProvider>
+      </AppContextProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
