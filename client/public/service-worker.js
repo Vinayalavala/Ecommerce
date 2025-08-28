d76020b2854aec172eff065b98ecb6bed7db5c84
Vinay alavala <147227489+Vinayalavala@users.noreply.github.com>
2025-08-28 16:48:45 +0530

    Add files via upload

---
+++ client/public/service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installing...");
+  event.waitUntil(
+    caches.open("ecommerce-cache-v1").then((cache) => {
+      return cache.addAll(["/"]);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activated.");
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
