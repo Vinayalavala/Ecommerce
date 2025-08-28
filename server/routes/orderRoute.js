bbc69dd18413b40eadee57d77ae0702fc1104df1
Vinay alavala <147227489+Vinayalavala@users.noreply.github.com>
2025-08-28 15:00:34 +0530

    Update orderRoute.js

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,26 +1,29 @@
-import express from 'express';
-import authUser from '../middlewares/authUser.js';
-import authSeller from '../middlewares/authSeller.js';
+import express from "express";
+import authUser from "../middlewares/authUser.js";
+import authSeller from "../middlewares/authSeller.js";
 import {
   getUserOrders,
   placeOrderCOD,
+  createRazorpayOrder,
+  verifyRazorpayPayment,
   getAllOrders,
-  placeOrderStripe,
-  markOrderAsPaid,
   updateOrderStatus,
-  cancelOrder
-} from '../controllers/orderController.js';
+  cancelOrder,
+} from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
-orderRouter.post('/cod', authUser, placeOrderCOD);
-orderRouter.get('/user', authUser, getUserOrders);
-orderRouter.get('/seller', authSeller, getAllOrders);
-orderRouter.post('/stripe', authUser, placeOrderStripe);
-orderRouter.patch('/:orderId/mark-paid', markOrderAsPaid);
-orderRouter.put('/:orderId/status', authSeller, updateOrderStatus);
-// PUT /api/order/cancel/:id
-orderRouter.put('/cancel/:id', cancelOrder);
+// COD
+orderRouter.post("/cod", authUser, placeOrderCOD);
 
+// Razorpay
+orderRouter.post("/razorpay/create-order", authUser, createRazorpayOrder);
+orderRouter.post("/razorpay/verify", authUser, verifyRazorpayPayment);
+
+// Orders
+orderRouter.get("/user", authUser, getUserOrders);
+orderRouter.get("/seller", authSeller, getAllOrders);
+orderRouter.put("/:orderId/status", authSeller, updateOrderStatus);
+orderRouter.put("/cancel/:id", cancelOrder);
 
 export default orderRouter;
