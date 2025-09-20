aa4a62efb96c667293492184a7b5c4676f966c27
Vinay alavala <147227489+Vinayalavala@users.noreply.github.com>
2025-09-20 10:44:14 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -90,7 +90,7 @@ const App = () => {
           <Route path="/loader" element={<Loading />} />
 
           <Route path="/seller" element={isSeller ? <SellerLayout /> : <SellerLogin />}>
-            <Route index element={isSeller ? <AddProduct /> : null} />
+            <Route index element={isSeller ? <Orders /> : null} />
             <Route path="product-list" element={<ProductList />} />
             <Route path="orders" element={<Orders />} />
             <Route path="analytics" element={<Analytics />} />
